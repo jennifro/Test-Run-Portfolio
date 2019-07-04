ecb5c94dbaef65b45caac129986727b32d07afc0
Jennifer <jen.dxon@gmail.com>
2019-07-04 15:43:53 -0700

    Updates

---
+++ precache-manifest.cb0b7e7dcbbe15e1c4d652a2b7340009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9eeaca710faa37f2073a302c489952",
+    "url": "/Test-Run-Portfolio/index.html"
+  },
+  {
+    "revision": "f97547fe3945dfbfd3a0",
+    "url": "/Test-Run-Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Test-Run-Portfolio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f97547fe3945dfbfd3a0",
+    "url": "/Test-Run-Portfolio/static/js/main.76e6404a.chunk.js"
+  },
+  {
+    "revision": "8b4d48ffb2d004cd31fc",
+    "url": "/Test-Run-Portfolio/static/js/runtime~main.d7e1e618.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Test-Run-Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
